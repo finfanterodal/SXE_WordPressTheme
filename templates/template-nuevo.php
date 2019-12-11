a9c6b30119d792445ef3f9fdbec5a1c404a9f238
EC2 Default User <ec2-user@ip-172-31-24-184.eu-west-1.compute.internal>
2019-12-11 09:49:49 +0000

    Cambio Footer-nuevo en el nuevo template.

--- templates/template-nuevo.php
+++ templates/template-nuevo.php
@@ -7,35 +7,27 @@ Template Post Type: post, page, event
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
-		<?php
-		// Start the loop.
-		while ( have_posts() ) :
-			the_post();
-			the_title();
-			the_time();
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
-			// Include the page content template.
-			get_template_part( 'template-parts/content', 'page' );
+			<?php
+			while ( have_posts() ) :
+				the_post();
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) {
-				comments_template();
-			}
-				?>
-				
-				<h1>Estoy en el Template nuevo</h1>
-					<?php
-			// End of the loop.
-		endwhile;
-		?>
+				get_template_part( 'template-parts/page/content', 'page' );
 
-	</main><!-- .site-main -->
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
 
-	<?php get_sidebar( 'content-bottom' ); ?>
+			endwhile; // End of the loop.
+			?>
 
-</div><!-- .content-area -->
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php
+get_footer('nuevo');
